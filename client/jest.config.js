fd37774eac269c79987b5872782b64c6f5f53826
Matt Newhall <matt@newhall.dev>
2025-09-15 23:24:32 +0100

    test: add unit tests for boardUtils

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.svg$': '<rootDir>/svg_transform.js'
+    '^.+\\.tsx?$': 'ts-jest'
   }
 }
