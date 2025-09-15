7478f41fb6d8fd0f6ff9d7f40abfa376a8f00200
Matt Newhall <matt@newhall.dev>
2025-09-15 21:43:16 +0100

    feat: basic play ability

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,14 +9,44 @@ export const Main = () => {
     [undefined, undefined, undefined]
   ])
 
-  return <div className='flex flex-col mt-10 items-center gap-10'>
-    <div className='font-bold text-2xl'>Tic Tac Toe</div>
-    <div className='flex flex-col gap-1'>
-      {board.map(row => <div className='flex gap-1'>
-        {row.map(column => <div className='border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex'>
-          {column}
-        </div>)}
-      </div>)}
+  const [player, setPlayer] = useState<XorO>('X')
+
+  const onClickCell = (rowIndex: number, colIndex: number) => {
+    if (board[rowIndex][colIndex] !== undefined) {
+      return
+    }
+
+    const updatedBoard = board.map((row, rowId) =>
+      row.map((cell, colId) => {
+        if (rowId === rowIndex && colId === colIndex) {
+          return player
+        }
+        return cell
+      })
+    )
+
+    setBoard(updatedBoard)
+    setPlayer(player === 'X' ? 'O' : 'X')
+  }
+
+  return (
+    <div className='flex flex-col mt-10 items-center gap-10'>
+      <div className='font-bold text-2xl'>Tic Tac Toe</div>
+      <div className='flex flex-col gap-1'>
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className='flex gap-1'>
+            {row.map((col, colIndex) => (
+              <div
+                key={colIndex}
+                className='border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex hover:bg-gray-100'
+                onClick={() => onClickCell(rowIndex, colIndex)}
+              >
+                {col}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
+  )
 }
